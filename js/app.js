b3ae093cdbf12739e7af1c3bdead5b8443a9c1de
Niesha S. (nyestanley) <nieshaamos@gmail.com>
2015-04-23 16:01:45 -0400

    Create enemy instance and modify Enemy update function

--- js/app.js
+++ js/app.js
@@ -6,9 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.locx=0;
-    this.locy=0;
-    this.speed=5;
+    this.x=0;
+    this.y=0;
+    this.speed=1;
 }
 
 // Update the enemy's position, required method for game
@@ -17,8 +17,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    locx *= dt;
-    locy *= dt;
+    this.x = (this.x + this.speed);
+    //this.y = (this.x + this.speed) * dt;
+    //this.render();
 }
 
 // Draw the enemy on the screen, required method for game
@@ -30,9 +31,9 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function(){
-    this.sprite = 'images/char-princess-girl.png';
-    this.locx = 2;
-    this.locy = 4;
+    this.sprite = 'images/char-boy.png';
+    this.x = 400;
+    this.y = 400;
 }
 
 // Update the player's position, required method for game
@@ -41,24 +42,36 @@ Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.locx *= dt;
-    this.locy *= dt;
+    this.x = this.x * dt;
+    //this.y *= dt;
 }
 
-// Draw the enemy on the screen, required method for game
+// Draw the player on the screen, required method for game
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-// Draw the enemy on the screen, required method for game
+
 Player.prototype.handleInput = function(allowedKeys) {
     
 }
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
 var player = new Player();
+
+var allEnemies = [];
+var enemy1 = new Enemy();
+allEnemies.push(enemy1);
+/*var enemy2 = new Enemy();
+allEnemies.push(enemy1);
+var enemy3 = new Enemy();
+allEnemies.push(enemy1);
+var enemy4 = new Enemy();
+allEnemies.push(enemy1);
+var enemy5 = new Enemy();
+allEnemies.push(enemy1);*/
+
 
 
 // This listens for key presses and sends the keys to your
