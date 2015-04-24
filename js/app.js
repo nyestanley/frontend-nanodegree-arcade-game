f953a5b3108a04ea5e6393af8e6713442d727025
Niesha S. (nyestanley) <nieshaamos@gmail.com>
2015-04-24 15:59:25 -0400

    Finish Enemy methods implementation, Still working on Player class

--- js/app.js
+++ js/app.js
@@ -6,9 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x=0;
-    this.y=0;
-    this.speed=1;
+    this.x=-110;
+    this.setEnemyCoordinateY();
+    this.setEnemySpeed()
 }
 
 // Update the enemy's position, required method for game
@@ -27,13 +27,26 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+//Set enemy initital Y coordinate
+Enemy.prototype.setEnemyCoordinateY = function(){
+    var rowObject=[60, 145, 230]; 
+    this.y = rowObject[Math.floor(Math.random()*3)];
+}
+
+//Set enemy speed
+Enemy.prototype.setEnemySpeed = function(){
+    this.speed = Math.random()*9;
+}
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function(){
     this.sprite = 'images/char-boy.png';
-    this.x = 400;
-    this.y = 400;
+    this.x = 200;
+    this.y = 380;
+    this.moveX=0;
+    this.moveY=0;
 }
 
 // Update the player's position, required method for game
@@ -42,8 +55,19 @@ Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x * dt;
-    //this.y *= dt;
+   /* if(this.moveX!=0){
+        this.x+=this.moveX;
+    }
+    if(this.moveY!=0){
+        this.y+=this.moveY;
+    }
+*/
+
+    console.log("Y move " + this.moveY);
+    this.y = this.y+this.moveY;
+    this.resetPlayerMoves();
+    
+    
 }
 
 // Draw the player on the screen, required method for game
@@ -53,24 +77,42 @@ Player.prototype.render = function() {
 
 
 Player.prototype.handleInput = function(allowedKeys) {
+    console.log(allowedKeys);
+
+    if(allowedKeys='up'){
+        this.moveY=-65;
+        console.log(this.moveY);
+    }
+    if(allowedKeys='down'){
+        this.moveY=65;
+    }
+   /* if(allowedKeys='left'){
+        this.moveX=-65;
+    }
+    if(allowedKeys='right'){
+        this.moveX=65;
+    }*/
+
+    console.log(this.y);
     
 }
+
+Player.prototype.resetPlayerMoves=function(){
+    this.moveX = 0;
+    this.moveY = 0;
+}
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var player = new Player();
-
 var allEnemies = [];
-var enemy1 = new Enemy();
-allEnemies.push(enemy1);
-/*var enemy2 = new Enemy();
-allEnemies.push(enemy1);
-var enemy3 = new Enemy();
-allEnemies.push(enemy1);
-var enemy4 = new Enemy();
-allEnemies.push(enemy1);
-var enemy5 = new Enemy();
-allEnemies.push(enemy1);*/
+
+for(var i=1; i<=7; i++){
+
+//var enemy1 = new Enemy();
+allEnemies.push(new Enemy());
+}
 
 
 
